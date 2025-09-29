39e8fb134cda2721ae3f194bcc0cb3d7934a06de
kavinduUdhara <wmkavinduudhara@gmail.com>
2025-09-30 00:13:17 +0530

    made versatiyle for 3 codons | assesment completed

--- tagFinder2/DnaTagFinder.java
+++ tagFinder2/DnaTagFinder.java
@@ -1,7 +1,10 @@
+import edu.duke.FileResource;
+
 public class DnaTagFinder {
   public static void main(String[] args){
+    FileResource fr = new FileResource("Axl2p.fa");
     DnaTagFinder tf = new DnaTagFinder();
-    String portion = tf.findProtein("cccatggggtttaaataataataggagagagagagagagttt");
+    String portion = tf.findProtein(fr.asString());
     System.out.println(portion);
     System.out.println(portion.length());
   }
@@ -17,10 +20,19 @@ public class DnaTagFinder {
     if (end == -1){
       return tag;
     }
-    if ((end - start) % 3 != 0){
-      return tag;
+    while ((end - start) % 3 != 0){
+      end = dna.indexOf("tag", end + 3);
+      // if((end - start) % 3 == 0){
+      //   tag = dna.substring(start, end);
+      //   return tag;
+      // }
+      if (end == -1){
+        return tag;
+      }
     }
     tag = dna.substring(start, end+3);
     return tag;
   }
+
 }
+
